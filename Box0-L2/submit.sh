9981fc2f276ce4be29584f02056a91208890b041
dcarrel1 <58124391+dcarrel1@users.noreply.github.com>
2021-07-05 13:33:15 -0700

    Update submit.sh

--- Box0-L2/submit.sh
+++ Box0-L2/submit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH -J Box0-L1
-#SBATCH -o Box0-L1.out
-#SBATCH -e Box0-L1.err
+#SBATCH -J Box0-L2
+#SBATCH -o Box0-L2.out
+#SBATCH -e Box0-L2.err
 #SBATCH -N 2
-#SBATCH -n 12
+#SBATCH -n 24
 #SBATCH -p development
 #SBATCH -t 02:00:00
 #SBATCH --mail-user=carrel@ucsb.edu
@@ -13,4 +13,4 @@ module load hdf5
 module load mvapich2
 
 cd $SLURM_SUBMIT_DIR
-ibrun /work2/08137/tg874363/stampede2/Box0/enzo.exe -d Box0-L1.enzo
+ibrun /work2/08137/tg874363/stampede2/Box0/enzo.exe -d Box0-L2.enzo
